eb36e8158d9358b27c4bc927fb62ec2a8809cf43
mpodles <mp@kpodles.pl>
2019-02-12 19:50:43 +0100

    getAllChildren function skeleton added. SAP object declaration in main added.

--- src/main/java/com/ammp/dp/DpApplication.java
+++ src/main/java/com/ammp/dp/DpApplication.java
@@ -1,5 +1,6 @@
 package com.ammp.dp;
 
+import com.ammp.dp.actions.SaveAccessProtector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +9,6 @@ public class DpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DpApplication.class, args);
+		SaveAccessProtector gówno = new SaveAccessProtector("harryweb.atthost24.pl","1404_wzorce","Wz0rce2018","mysql");
 	}
 }
--- src/main/java/com/ammp/dp/actions/SaveAccessProtector.java
+++ src/main/java/com/ammp/dp/actions/SaveAccessProtector.java
@@ -4,7 +4,10 @@ import com.ammp.dp.factory.*;
 import com.ammp.dp.factory.MySQL.MySQLFactory;
 import com.ammp.dp.factory.PSQL.PostgreSQLFactory;
 
+import java.sql.ResultSet;
 import java.sql.SQLInput;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -13,23 +16,41 @@ public class SaveAccessProtector {
     private Commit commit;
     private Connection connection;
     private Statement statement;
-
     private SQLFactory sqlFactory;
+    private ArrayList<Integer> children;
+    private int userID;
 
     public SaveAccessProtector(String hostname, String user, String password, String dbType) {
-
-        if(dbType.equals(Constants.POSTGRESQL)){
+        if (dbType.equals(Constants.POSTGRESQL)) {
             sqlFactory = new PostgreSQLFactory();
-        } else if (dbType.equals(Constants.MYSQL)){
+        } else if (dbType.equals(Constants.MYSQL)) {
             sqlFactory = new MySQLFactory();
         }
 
         connection = Objects.requireNonNull(sqlFactory).createConenction(hostname, user, password);
     }
 
-    public List<Object> selectFromDB(SQLInput sqlInput, long userId){
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public List<Object> selectFromDB(SQLInput sqlInput, long userId) {
         return null;
     }
 
-    java.sql.Connection conn = connection.getConnection();
+    private void getAllChildren(int id) throws java.sql.SQLException {
+        String query = "SELECT * FROM roles WHERE RoleID="+Integer.toString(id);
+
+        // create the java statement
+        Statement st = conn.createStatement();
+
+        // execute the query, and get a java resultset
+        ResultSet rs = st.executeQuery(query);
+        while (rs.next()) {
+            int ChildID = rs.getInt("ChildID");
+
+        }
+    }
+        java.sql.Connection conn = connection.getConnection();
+
 }
