69d048f09a2c2d0b1ab3df8c7bfdae3a235fb590
Piotr <piotrwiklinski@gmail.com>
2019-02-13 01:04:19 +0100

    Fixed constructor error

--- src/main/java/com/ammp/dp/DpApplication.java
+++ src/main/java/com/ammp/dp/DpApplication.java
@@ -11,6 +11,7 @@ public class DpApplication {
         SaveAccessProtector protecc = SaveAccessProtector.getInstance();
         protecc.connect("harryweb.atthost24.pl", "1404_wzorce", "1404_wzorce", "Wz0rce2018", Constants.MYSQL);
         protecc.setUserID("3");
+        protecc.execute("select * from example_table where Data is not null;");
 //		SpringApplication.run(DpApplication.class, args);
     }
 }
--- src/main/java/com/ammp/dp/factory/MySQL/MySQLFactory.java
+++ src/main/java/com/ammp/dp/factory/MySQL/MySQLFactory.java
@@ -19,6 +19,6 @@ public class MySQLFactory extends SQLFactory {
 
     @Override
     public Commit createCommit(Connection connection) {
-        return new MySQLCommit(connection);
+        return new MySQLCommit();
     }
 }
