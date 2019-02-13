305187028ab4b033484b3edffba82a2459e14018
Piotr <piotrwiklinski@gmail.com>
2019-02-13 10:58:49 +0100

    Regex-based query extender

---
+++ src/main/java/com/ammp/dp/QueryExtender.java
@@ -0,0 +1,72 @@
+package com.ammp.dp;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class QueryExtender {
+    private static final String regexNoWhere = "(select [a-zA-Z\\*]+(, )*[A-Z,a-z]* from [A-Za-z_]+[ ]*)(.*)";
+    private static final Pattern patternNoWhere = Pattern.compile(regexNoWhere);
+    private static final String regexWhere = "(select [a-zA-Z\\*]+(, )*[A-Z,a-z]* from [A-Za-z_]+) (where ([a-zA-Z]+ (= [a-zA-Z0-9]+|> [a-zA-Z0-9]+|< [a-zA-Z0-9]+|>= [a-zA-Z0-9]+|<= [a-zA-Z0-9]+|is null|is not null)( and | or )*)+)(.*)";
+    private static final Pattern patternWhere = Pattern.compile(regexWhere);
+
+    private static ArrayList<String> getTestStrings() {
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("select * from users;");
+        strings.add("select * from users order BY id;");
+        strings.add("select id from users;");
+        strings.add("select id from users order BY id;");
+        strings.add("select id, name from users;");
+        strings.add("select id, name from users order BY id;");
+        strings.add("select * from users where id = 15;");
+        strings.add("select * from users where id = 12 ORDER BY id;");
+        strings.add("select id from users where id = 1;");
+        strings.add("select id from users where id = 1 ORDER BY id;");
+        strings.add("select id, name from users where id = 10;");
+        strings.add("select id, name from users where id = 12 ORDER BY id;");
+        strings.add("select id, name from users where id = 14 or id = 2;");
+        strings.add("select id, name from users where id = 14 and id >= 2;");
+        strings.add("select id, name from users where id >= 14 or id = 2;");
+        strings.add("select id, name from users where id = 16 or id = 2 ORDER BY id;");
+        strings.add("select id, name from users where id = 16 or id is null ORDER BY id;");
+        strings.add("select id, name from users where id = 16 or id is not null ORDER BY id;");
+        strings.add("select id, name from users where id is null and id is not null ORDER BY id;");
+        return strings;
+    }
+
+    public static String addWhere(String input, String condition) {
+        Matcher matcher = patternNoWhere.matcher(input);
+        if (matcher.matches()) {
+            return matcher.group(1) + " where (" + condition + ") " + matcher.group(3);
+        }
+        return "Error";
+    }
+
+    public static String extendWhere(String input, String condition) {
+        Matcher matcher = patternWhere.matcher(input);
+        if (matcher.matches()) {
+            return matcher.group(1) + " where (" + condition + ") and (" + matcher.group(3).replace("where ", "") + ")" + matcher.group(7);
+
+        }
+        return "Error";
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> strings = getTestStrings();
+        ArrayList<String> output = new ArrayList<>();
+        String conditionString = "id < 0 and id in (0,1,2)";
+        for (String s : strings) {
+            if (s.toLowerCase().contains("where"))
+                output.add(extendWhere(s, conditionString));
+            else
+                output.add(addWhere(s, conditionString));
+        }
+        for (int i = 0; i < strings.size(); i++) {
+            System.out.println("In: " + strings.get(i));
+            System.out.println("Out: " + output.get(i));
+        }
+    }
+
+
+}
